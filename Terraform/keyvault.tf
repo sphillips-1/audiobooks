0e1029b767786573e08d672af45986a3725222db
Sean <sean-phillips@live.com>
2025-08-31 11:17:01 -0400

    Fix access policy object_id reference for app service identity

--- Terraform/keyvault.tf
+++ Terraform/keyvault.tf
@@ -12,7 +12,7 @@ resource "azurerm_key_vault" "kv" {
 resource "azurerm_key_vault_access_policy" "app_service" {
     key_vault_id = azurerm_key_vault.kv.id
     tenant_id    = data.azurerm_client_config.current.tenant_id
-    object_id    = azurerm_app_service.app.identity.principal_id
+    object_id    = azurerm_app_service.app.identity[0].principal_id
 
     secret_permissions = [
         "get",
