75a3e57e92a40b05e3d02d032aaf6ae8b255445a
Sean <sean-phillips@live.com>
2025-08-31 11:32:07 -0400

    Change app service plan SKU to Free tier

--- Terraform/appservice.tf
+++ Terraform/appservice.tf
@@ -3,8 +3,8 @@ resource "azurerm_app_service_plan" "asp" {
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     sku {
-        tier = "Basic"
-        size = "B1"
+        tier = "Free"
+        size = "F1"
     }
 }
 
