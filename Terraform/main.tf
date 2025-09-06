326d7ac95103fa04df06ef59507604078d9c08bb
Sean <sean-phillips@live.com>
2025-09-06 19:39:51 -0400

    fixed terraform version

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = ">= 1.1.0"
+    required_version = "~> 1.13.0"
     backend "azurerm" {
         storage_account_name  = "satfstateeast"
         container_name        = "tfstate-audiobooks"
