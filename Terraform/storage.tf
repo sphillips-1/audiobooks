aaea48feddf3aa370a3347982724edbd22277391
Sean <sean-phillips@live.com>
2025-09-07 19:20:52 -0400

    updated sotrage account

--- Terraform/storage.tf
+++ Terraform/storage.tf
@@ -2,20 +2,23 @@ resource "azurerm_storage_account" "main" {
   name                     = "saaudiobooksean"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
+
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 
+  # Required to support >2MB files
   large_file_share_enabled = true
 }
 
 resource "azurerm_storage_share" "media" {
-  name                 = "abs-media"
+  name               = "abs-media"
   storage_account_id = azurerm_storage_account.main.id
-  quota                = 5120 # GB
+  quota              = 5120  # 5 TB for audiobooks
 }
 
 resource "azurerm_storage_share" "config" {
-  name                 = "abs-config"
+  name               = "abs-config"
   storage_account_id = azurerm_storage_account.main.id
-  quota                = 100
+  quota              = 100  # small, just for metadata/db
 }
