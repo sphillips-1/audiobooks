5a6ab8a041e34f66602050bcda2ef178d9e5a270
Sean <sean-phillips@live.com>
2025-09-08 16:11:43 -0400

    enabled_protocol

--- Terraform/storage.tf
+++ Terraform/storage.tf
@@ -9,12 +9,14 @@ resource "azurerm_storage_account" "main" {
 
   # Required to support >2MB files
   large_file_share_enabled = true
+
 }
 
 resource "azurerm_storage_share" "media" {
   name               = "abs-media"
   storage_account_id = azurerm_storage_account.main.id
   quota              = 5120  # 5 TB for audiobooks
+  enabled_protocol     = "SMB" # ensure SMB access is enabled
 }
 
 resource "azurerm_storage_share" "config" {
