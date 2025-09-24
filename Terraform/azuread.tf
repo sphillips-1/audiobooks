67aa1ad371f51691e195e26bca035b0a7e3da78a
Sean <sean-phillips@live.com>
2025-09-24 09:00:45 -0400

    Refactor redirect_uris and logout_url for consistency and clarity

--- Terraform/azuread.tf
+++ Terraform/azuread.tf
@@ -9,10 +9,10 @@ resource "azuread_application" "abs" {
   # ✅ OIDC web app configuration with both web and mobile redirect URIs
   web {
     redirect_uris = [
-      "https://${azurerm_container_group.aci.fqdn}/auth/openid/callback", # Replace with your ABS URL
-      "audiobookshelf://auth"                        # Mobile app redirect
+      "https://${azurerm_container_group.aci.fqdn}/auth/openid/callback",
+      "audiobookshelf://auth"
     ]
-    logout_url = join("", [azurerm_container_group.aci.fqdn,"/logout"])
+    logout_url = "https://${azurerm_container_group.aci.fqdn}/logout"
   }
 
   # Optional: Request basic Microsoft Graph permissions
