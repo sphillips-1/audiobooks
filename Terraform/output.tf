58594ff9ea3b846f10d58fcabfefbb23ce115150
Sean <sean-phillips@live.com>
2025-09-26 15:53:34 -0400

    Add output for Audiobookshelf redirect URI in Terraform configuration

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -16,3 +16,8 @@ output "abs_tenant_id" {
   description = "Tenant ID (used in Issuer URL)"
   value       = data.azuread_client_config.current.tenant_id
 }
+
+output "abs_redirect_uri" {
+  description = "Redirect URI for Audiobookshelf (used in OIDC settings)"
+  value       = "https://${azurerm_container_group.aci.dns_name_label}.${azurerm_container_group.aci.location}.azurecontainer.io/auth/openid/callback"
+}
