477912cbb18720aa6fb42ff9b61ca71c4ab73cb4
Sean <sean-phillips@live.com>
2025-09-27 12:13:10 -0400

    Rename resource for Azure Container Instance configuration to improve clarity

--- Terraform/aci-ssl-complete.tf
+++ Terraform/aci-ssl-complete.tf
@@ -1,4 +1,4 @@
-resource "azurerm_container_group" "audiobookshelf" {
+resource "azurerm_container_group" "aci" {
   name                = "audiobookshelf-aci"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
