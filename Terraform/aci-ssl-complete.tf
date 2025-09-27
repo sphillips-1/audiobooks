8f1556420f2d8e4a6c848e6fa56103a7f6095dbd
Sean <sean-phillips@live.com>
2025-09-27 12:36:28 -0400

    Update NGINX image reference to use the Azure Linux base image

--- Terraform/aci-ssl-complete.tf
+++ Terraform/aci-ssl-complete.tf
@@ -8,7 +8,7 @@ resource "azurerm_container_group" "aci" {
 
   container {
     name   = "nginx"
-    image  = "mcr.microsoft.com/nginx/nginx:latest"
+    image  = "mcr.microsoft.com/azurelinux/base/nginx:latest"
     cpu    = "0.1"
     memory = "0.2"
     ports {
