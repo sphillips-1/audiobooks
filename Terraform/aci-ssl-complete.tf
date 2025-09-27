e53f52281ba5d62a628738b39afba1da29f8d91a
Sean <sean-phillips@live.com>
2025-09-27 12:22:33 -0400

    Update NGINX image reference to use the Microsoft Container Registry

--- Terraform/aci-ssl-complete.tf
+++ Terraform/aci-ssl-complete.tf
@@ -8,7 +8,7 @@ resource "azurerm_container_group" "aci" {
 
   container {
     name   = "nginx"
-    image  = "nginx:latest"
+    image  = "mcr.microsoft.com/nginx/nginx:latest"
     cpu    = "0.1"
     memory = "0.2"
     ports {
