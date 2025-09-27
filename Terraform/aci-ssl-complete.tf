f66578bac6d55b1a5a6f9d4e7e055bbf02c64eeb
Sean <sean-phillips@live.com>
2025-09-27 09:54:42 -0400

    Add Azure Container Instance configuration for audiobookshelf with NGINX proxy and SSL support

---
+++ Terraform/aci-ssl-complete.tf
@@ -0,0 +1,54 @@
+resource "azurerm_container_group" "audiobookshelf" {
+  name                = "audiobookshelf-aci"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  ip_address_type     = "Public"
+  dns_name_label      = "audiobookshelf-ssl"
+
+  container {
+    name   = "nginx"
+    image  = "nginx:latest"
+    cpu    = "0.1"
+    memory = "0.2"
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+    volume {
+      name                 = "nginx-config"
+      mount_path           = "/etc/nginx/conf.d"
+      share_name           = azurerm_storage_share.nginx_config.name
+      storage_account_name = azurerm_storage_account.main.name
+      storage_account_key  = azurerm_storage_account.main.primary_access_key
+      read_only            = false
+    }
+    volume {
+      name                 = "nginx-cert"
+      mount_path           = "/etc/nginx/certs"
+      share_name           = azurerm_storage_share.nginx_cert.name
+      storage_account_name = azurerm_storage_account.main.name
+      storage_account_key  = azurerm_storage_account.main.primary_access_key
+      read_only            = false
+    }
+  }
+
+  container {
+    name   = "audiobookshelf"
+    image  = "ghcr.io/advplyr/audiobookshelf:latest"
+    cpu    = "0.1"
+    memory = "0.2"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+
+
+  # Volumes are now defined inside each container block below
+  tags = { app = "audiobookshelf" }
+}
+
+# Place your SSL cert and nginx.conf in the Azure File Share.
+# nginx.conf should proxy HTTPS to audiobookshelf container on port 80.
--- Terraform/storage.tf
+++ Terraform/storage.tf
@@ -24,3 +24,15 @@ resource "azurerm_storage_share" "config" {
   storage_account_id = azurerm_storage_account.main.id
   quota              = 100  # small, just for metadata/db
 }
+
+resource "azurerm_storage_share" "nginx_config" {
+  name               = "nginx-config"
+  storage_account_id = azurerm_storage_account.main.id
+  quota              = 10   # small, just for nginx.conf
+}
+
+resource "azurerm_storage_share" "nginx_cert" {
+  name               = "nginx-cert"
+  storage_account_id = azurerm_storage_account.main.id
+  quota              = 10   # small, just for SSL certs
+}
